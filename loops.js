b532e9b7158ceb7311293ffcdcb1c9ad583d46df
Victor Romero <romeroa.victor@yahoo.com>
2020-07-04 21:42:07 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,6 +15,10 @@ function whileLoop(number){
   }
     return 'done';
 }
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
 function doWhileLoop(int){
   do{ 
     console.log("I run once regardless.");
